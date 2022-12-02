dc177bb2a0589412ff2ff3d6ad8d54bfb881a37e
Max Edwards <youwontforgetthis@gmail.com>
2022-12-02 16:37:09 +0000

    wind rose is displaying curved text

--- components/WindRose.tsx
+++ components/WindRose.tsx
@@ -98,8 +98,6 @@ const WindRose = ({ sites }: { sites: Array<siteType> }) => {
     }
   }
 
-  console.log(bins);
-
   const size = 600;
   const draw = (ctx: any, x: number, y: number) => {
     const center = size / 2;
@@ -140,11 +138,126 @@ const WindRose = ({ sites }: { sites: Array<siteType> }) => {
           ctx.fillStyle = "black";
           ctx.font = "20px Arial";
           ctx.fillText(site.name, 200, size - 200);
+        } else {
+          /* if (site.name !== "Corndon") continue; */
+          ctx.font = "20px Arial";
+          ctx.fillStyle = "red";
+          drawTextAlongArc(
+            ctx,
+            site.name,
+            center,
+            center,
+            center - radiusOffset - 15,
+            calculateMidAngle(cardinalAngles[site.directionFrom], cardinalAngles[site.directionTo]),
+            "Arial"
+          );
+          console.log(site.name);
+          console.log(calculateMidAngle(cardinalAngles[site.directionFrom], cardinalAngles[site.directionTo]));
         }
       }
     }
   };
+  function calculateMidAngle(start: number, end: number) {
+    start += 90;
+    end += 90;
+    if (start > 359) start = start - 360;
+    if (end > 359) end = end - 360;
 
+    let midPoint = 0;
+    if (end > start) {
+      midPoint = (end - start) / 2 + start;
+    } else {
+      midPoint = start + (360 - (start - end)) / 2;
+    }
+    if (midPoint > 359) {
+      midPoint = midPoint - 360;
+    }
+    return midPoint;
+  }
+  // Arc Length = angle × (Math.PI/180) × radius
+  function drawTextAlongArc(
+    ctx: any,
+    text: string,
+    centerX: number,
+    centerY: number,
+    radius: number,
+    startAngle: number,
+    fName: string
+  ) {
+    // text:         The text to be displayed in circular fashion
+    // diameter:     The diameter of the circle around which the text will
+    //               be displayed (inside or outside)
+    // startAngle:   In degrees, Where the text will be shown. 0 degrees
+    //               if the top of the circle
+    // align:        Positions text to left right or center of startAngle
+    // textInside:   true to show inside the diameter. False to show outside
+    // inwardFacing: true for base of text facing inward. false for outward
+    // fName:        name of font family. Make sure it is loaded
+    // fSize:        size of font family. Don't forget to include units
+    // kearning:     0 for normal gap between letters. positive or
+    //               negative number to expand/compact gap in pixels
+    //------------------------------------------------------------------------
+
+    // declare and intialize canvas, reference, and useful variables
+    //
+    ctx.save();
+    const fSize = "14pt";
+    const kerning = 0;
+
+    const align = "center";
+    var clockwise = 1; // draw clockwise for aligned right. Else Anticlockwise
+    startAngle = startAngle * (Math.PI / 180); // convert to radians
+
+    // calculate height of the font. Many ways to do this
+    // you can replace with your own!
+    var div = document.createElement("div");
+    div.innerHTML = text;
+    div.style.position = "absolute";
+    div.style.top = "-10000px";
+    div.style.left = "-10000px";
+    div.style.fontFamily = fName;
+    div.style.fontSize = fSize;
+    document.body.appendChild(div);
+    var textHeight = div.offsetHeight;
+    document.body.removeChild(div);
+
+    ctx.fillStyle = "red";
+    ctx.font = fSize + " " + fName;
+
+    // Setup letters and positioning
+    ctx.translate(centerX, centerY); // Move to center
+    ctx.textBaseline = "middle"; // Ensure we draw in exact center
+    ctx.textAlign = "center"; // Ensure we draw in exact center
+
+    // rotate 50% of total angle for center alignment
+    if (align == "center") {
+      for (var j = 0; j < text.length; j++) {
+        var charWid = ctx.measureText(text[j]).width;
+        startAngle += ((charWid + (j == text.length - 1 ? 0 : kerning)) / (radius - textHeight) / 2) * -clockwise;
+      }
+    }
+
+    // Phew... now rotate into final start position
+    ctx.rotate(startAngle);
+
+    // Now for the fun bit: draw, rotate, and repeat
+    for (var j = 0; j < text.length; j++) {
+      var charWid = ctx.measureText(text[j]).width; // half letter
+      // rotate half letter
+      ctx.rotate((charWid / 2 / (radius - textHeight)) * clockwise);
+      // draw the character at "top" or "bottom"
+      // depending on inward or outward facing
+      ctx.fillText(text[j], 0, 0 - radius);
+
+      ctx.rotate(((charWid / 2 + kerning) / (radius - textHeight)) * clockwise); // rotate half letter
+    }
+    ctx.restore();
+  }
+  /* function getFontSizeToFit(text: string, fontFace: string, maxWidth: number) {
+   *   const ctx = document.createElement('canvas').getContext('2d');
+   *   ctx.font = `1px ${fontFace}`;
+   *   return maxWidth / ctx.measureText(text).width;
+   * } */
   const onMouseMove = (e) => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
