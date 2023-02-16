078496dfcc6833a521ce674ae069e3869aaef34f
Max Edwards <youwontforgetthis@gmail.com>
2023-02-16 17:58:24 +0000

    added weather content

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -7,13 +7,13 @@ import { stripHtml } from "string-strip-html";
 import Date from "../../components/date";
 
 const pageProps = async (_: any) => {
-  const collectionData = await getAllCollectionData("content/news");
-  return { news: collectionData };
+  const newsPages = await getAllCollectionData("content/news");
+  const weatherPages = await getAllCollectionData("content/weather");
+  return { newsPages: newsPages, weatherPages: weatherPages };
 };
 export const getStaticProps = getBaseProps(pageProps);
 
-export default function Home({ baseProps, news }) {
-  console.log(news);
+export default function Home({ baseProps, newsPages, weatherPages }) {
   return (
     <Layout home navData={baseProps}>
       <Head>
@@ -25,11 +25,23 @@ export default function Home({ baseProps, news }) {
 
           <section>
             <h2>Recent Weather Updates</h2>
+            {weatherPages &&
+              weatherPages.map((item, index) => (
+                <div key={index}>
+                  <h3>
+                    <a href={"/weather/" + item.id}>{item.title}</a>
+                  </h3>
+                  <div>
+                    <Date date={item.date} />
+                  </div>
+                  <div>{stripHtml(item.contentHtml).result}</div>
+                </div>
+              ))}
           </section>
           <section>
             <h2>Recent News</h2>
-            {news &&
-              news.map((item, index) => (
+            {newsPages &&
+              newsPages.map((item, index) => (
                 <div key={index}>
                   <h3>
                     <a href={"/news/" + item.id}>{item.title}</a>
---
+++ pages/weather/[id].tsx
@@ -0,0 +1,50 @@
+import Layout from "../../components/layout";
+import { getAllCollectionIds, getCollectionData } from "../../lib/collection";
+import Head from "next/head";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
+import { GetStaticPaths } from "next";
+import { getBaseProps } from "lib/baseProps";
+
+export default function Site({
+  siteData,
+  baseProps,
+}: {
+  siteData: {
+    title: string;
+    date: Date;
+    contentHtml: string;
+  };
+  baseProps: object;
+}) {
+  return (
+    <Layout navData={baseProps}>
+      <Head>
+        <title>{siteData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{siteData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date date={siteData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: siteData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllCollectionIds("content/weather");
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+const pageProps = async ({ params }) => {
+  const siteData = await getCollectionData(params.id as string, "content/weather");
+  return {
+    siteData,
+  };
+};
+export const getStaticProps = getBaseProps(pageProps);
