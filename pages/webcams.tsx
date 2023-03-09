b29539e324ba3774fe3de063213196b3c0485783
Max Edwards <youwontforgetthis@gmail.com>
2023-03-09 19:29:16 +0000

    webcams starting to take shape

--- pages/webcams.tsx
+++ pages/webcams.tsx
@@ -9,7 +9,12 @@ export default function Webcams({ baseProps }: { baseProps: object }) {
     id: attributes.id as string,
     date: attributes.date as Date,
     title: attributes.title as string,
-    locations: attributes.locations as { name: string; description: string }[],
+    locations: attributes.locations as {
+      name: string;
+      description: string;
+      weatherStations: Array<{ station: string }>;
+      webcams: string;
+    }[],
   };
   return (
     <Layout navData={baseProps}>
@@ -19,21 +24,27 @@ export default function Webcams({ baseProps }: { baseProps: object }) {
       <article>
         <h1 className={utilStyles.headingXl}>{siteData.title}</h1>
         <WebcamContent />
-        {siteData.locations && siteData.locations.map((l) => <div>{l.name}</div>)}
-        {/* {siteData.weatherStations && (
-          <section>
-            <h2 id="weather">Weather Stations</h2>
-            {siteData.weatherStations.map((ws) => (
-              <div dangerouslySetInnerHTML={{ __html: ws.station }} />
-            ))}
-          </section>
-        )}
-        {siteData.webcams && (
-          <section>
-            <h2 id="webcams">Webcams</h2>
-            <div dangerouslySetInnerHTML={{ __html: webcamsState }} />
-          </section>
-        )} */}
+        {siteData.locations &&
+          siteData.locations.map((l) => (
+            <section>
+              <h2>{l.name}</h2>
+              <p>{l.description}</p>
+              {l.weatherStations && (
+                <section>
+                  <h3 id="weather">Weather Stations</h3>
+                  {l.weatherStations.map((ws) => (
+                    <div dangerouslySetInnerHTML={{ __html: ws.station }} />
+                  ))}
+                </section>
+              )}
+              {l.webcams && (
+                <section>
+                  <h3 id="webcams">Webcams</h3>
+                  <div dangerouslySetInnerHTML={{ __html: "some html" }} />
+                </section>
+              )}
+            </section>
+          ))}
       </article>
     </Layout>
   );
@@ -43,7 +54,7 @@ interface siteData {
   id: string;
   date: Date;
   title: string;
-  locations: { name: string }[];
+  locations: { name: string; description: string; weatherStations: Array<{ station: string }>; webcams: string }[];
 }
 
 const pageProps = async (_: any) => {
